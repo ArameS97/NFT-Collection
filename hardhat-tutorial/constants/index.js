b01bd3a82544d01fd49035d0185aade477ec27e4
Dmytro <dmiter33@gmail.com>
2022-09-08 17:28:42 +0300

    first commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x88563F024c7A41B77148B4Ad74eB8492962Ce697";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
